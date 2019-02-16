a82ed56caabd927b1f6c0218b55c1673943667ce
bhanuprakashbg <47267792+bhanuprakashbg@users.noreply.github.com>
2019-02-16 23:52:47 +0530

    Create javaJenkinsFile.groovy

---
+++ java/javaJenkinsFile.groovy
@@ -0,0 +1,19 @@
+def javastage() {
+  	 stage('Code Analysis') {
+			sh 'mvn sonar:sonar'
+		}
+		
+		stage('Code Coverage') {
+			sh 'mvn cobertura:cobertura'
+		}
+		
+		stage('Unit Testing') {
+			sh 'mvn test'
+		}
+
+		stage('Build') {    
+			sh 'mvn clean install '
+		}
+}
+
+return this
